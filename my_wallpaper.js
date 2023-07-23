eb0d17a27088d129b341906818bb3782d042eb89
FoxStone123 <140349953+FoxStone123@users.noreply.github.com>
2023-07-24 10:13:12 +1200

    copied into wallpaper

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,7 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-
+let eyeSize = 10; 
+  let headX = 199/2;
+  let headY = 199/2;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -19,5 +19,23 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  
+  fill(255); 
+  stroke(50, 153, 168); 
+
+  ellipse(80, 45, 20, 70) 
+  ellipse (120, 45, 20, 70)
+
+  ellipse(headX, headY, 80, 80); // draws a circle 45 pixels accross at location 100 pixels accross and 100 pixels down
+ 
+  arc(106.5, 120, 15, 10, 0, 180)
+  arc(91.5, 120, 15, 10, 0, 180) 
+
+  triangle(86, 109, 99, 120, 113, 109) 
+
+  fill(50, 194, 187)
+
+  ellipse(85, 89, eyeSize, eyeSize)
+  ellipse(115, 89, eyeSize, eyeSize) 
+
 }
