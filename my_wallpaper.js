7e8ef15ea613c55371338c0094d8d9923a84bec3
FoxStone123 <140349953+FoxStone123@users.noreply.github.com>
2023-07-27 13:45:37 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,10 +1,11 @@
 //your parameter variables go here!
-  let eyeSize = 10; 
-  let headX = 199/2;
-  let headY = 199/2;
+  let eyeSize = 5; 
+  let headX = 105;
+  let headY = 105;  
+ 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -23,13 +24,15 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   fill(255); 
   stroke(50, 153, 168); 
 
-  ellipse(80, 45, 20, 70) 
-  ellipse (120, 45, 20, 70)
+  ellipse(headX - 12, headY - 50, 20, 70) 
+  ellipse (headX + 12, headY - 50, 20, 70)
 
   ellipse(headX, headY, 80, 80); // draws a circle 45 pixels accross at location 100 pixels accross and 100 pixels down
  
   arc(106.5, 120, 15, 10, 0, 180)
   arc(91.5, 120, 15, 10, 0, 180) 
+
+  fill(194, 50, 98)
 
   triangle(86, 109, 99, 120, 113, 109) 
 
